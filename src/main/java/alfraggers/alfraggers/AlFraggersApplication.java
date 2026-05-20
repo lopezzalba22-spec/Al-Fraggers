c969a7f50e77eb1234f5cbbe22130c05b361f5a4
miXtolll <alogar89@gmail.com>
2026-05-20 10:41:43 +0200

    Primer commit: Estructura inicial de Spring Boot

---
+++ src/main/java/alfraggers/alfraggers/AlFraggersApplication.java
@@ -0,0 +1,13 @@
+package alfraggers.alfraggers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlFraggersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlFraggersApplication.class, args);
+    }
+
+}
---
+++ src/test/java/alfraggers/alfraggers/AlFraggersApplicationTests.java
@@ -0,0 +1,13 @@
+package alfraggers.alfraggers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlFraggersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
